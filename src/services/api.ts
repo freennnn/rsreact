fed9cb030172b9058f7f900b243cc04f1281da70
freen <freennnn@gmail.com>
2024-07-07 14:15:53 +0200

    feat: allitems default q parameter for github repos fetch

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
-const DEFAULT_SEARCH_QUERY = 'react'
+const DEFAULT_SEARCH_QUERY = 'allitems'
 const PATH_BASE = 'https://api.github.com'
 const PATH_SEARCH = '/search/repositories'
 const PARAM_SEARCH = 'q='
 const PARAMS_ADDITIONAL = '&sort=starts&page=1&per_page=3'
 
-// We set default value to searchTerm, cause github.api doesn't allow to fetch "all"
-// repositories, we fetch "all" react repos instead
-export async function getRepositores(searchTerm = DEFAULT_SEARCH_QUERY) {
-  return fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}${PARAMS_ADDITIONAL}`)
+// github.api doesn't allow to fetch unspecified 'all'
+// repositories - so we fetch 'allitems' repos instead
+export async function getRepositores(searchTerm: string) {
+  return fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm? searchTerm: DEFAULT_SEARCH_QUERY}${PARAMS_ADDITIONAL}`)
     .then(response => response.json())
 }
