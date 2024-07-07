68c9c1f98a7856dccf5c2e92abdbdc49e17409fa
freen <freennnn@gmail.com>
2024-07-07 15:16:33 +0200

    feat: add react-compiler eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-compiler', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+
   },
 }
