9de8ba9620fb8bc42f59e43f5e09b2f5c50e9386
freen <freennnn@gmail.com>
2024-07-26 20:09:15 +0200

    feat: tests added, rtk query + msw + redux store tested

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import pageNumberReducer from '../pages/GalleryPage/pageNumberSlice'
 import selectedItemsReducer from '../pages/GalleryPage/selectedItemsSlice'
@@ -13,9 +13,27 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 })
 
+// Tets:
+const rootReducer = combineReducers({
+  [githubApi.reducerPath]: githubApi.reducer,
+  pageNumber: pageNumberReducer,
+  selectedItems: selectedItemsReducer,
+})
+
+export function setupStore(preloadedState?: Partial<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
+  })
+}
+
 // Infer the type of `store`
-export type AppStore = typeof store
+//export type AppStore1 = typeof store
+export type AppStore = ReturnType<typeof setupStore>
+
 // Infer the `AppDispatch` type from the store itself
 export type AppDispatch = typeof store.dispatch
 // Same for the `RootState` type
-export type RootState = ReturnType<typeof store.getState>
+//export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
