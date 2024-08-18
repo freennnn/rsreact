7334f9e9309c1e9435cc7bc628fd250960aa01ed
freen <freennnn@gmail.com>
2024-08-18 07:57:24 +0200

    feat: react hook form basics

--- freennnnForms/src/pages/UserRegistrationHookForm/UserRegistrationHookFormPage.tsx
+++ freennnnForms/src/pages/UserRegistrationHookForm/UserRegistrationHookFormPage.tsx
@@ -1,12 +1,41 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 
+//import { User } from '../../data/types'
 import '../UserRegistrationFormPage.css'
 
+interface FormFields {
+  name: string
+  password: string
+}
+
 export default function UserRegistrationHookFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>()
+
+  const onSumbit: SubmitHandler<FormFields> = (data) => {
+    console.log(data)
+  }
   return (
     <div>
       <p>UserRegistrationHookFormPage</p>
       <Link to='/'> back to users lists</Link>
+      <form className='hook-form' onSubmit={handleSubmit(onSumbit)}>
+        <input
+          {...register('name', {
+            required: true,
+            validate: (value) => value.includes('@'),
+          })}
+          type='text'
+          placeholder='Name'
+        />
+        {errors.name && <div>Name field is not valid due to {errors.name.message}</div>}
+        <input {...register('password')} type='password' placeholder='Password' />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
