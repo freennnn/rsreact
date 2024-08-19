97208441948d4cb18f2d068f61da3c2559952c19
freen <freennnn@gmail.com>
2024-08-19 17:24:05 +0200

    feat: zod scheme updated, password validations changed to in-place, custom error for literal fields via some magic/hackery

--- freennnnForms/src/data/zodFormSchema.ts
+++ freennnnForms/src/data/zodFormSchema.ts
@@ -3,6 +3,57 @@ import { z } from 'zod'
 import { countryNames } from './countries'
 import { ACCEPTED_IMAGE_TYPES, MAX_IMAGE_SIZE } from './imageUtils'
 
+// if we would pass it to superRefine after the whole object - then we would need to specify 'path' fot ctx.addIssue
+function validatePasswordFieldInPlace(password: string, ctx: z.RefinementCtx) {
+  const containsUppercase = (ch: string) => /[A-Z]/.test(ch)
+  const containsLowercase = (ch: string) => /[a-z]/.test(ch)
+  const containsSpecialChar = (ch: string) => /[`!@#$%^&*()_\-+=[\]{};':"\\|,.<>/?~ ]/.test(ch)
+  let countOfUpperCase = 0,
+    countOfLowerCase = 0,
+    countOfNumbers = 0,
+    countOfSpecialChar = 0
+
+  for (let i = 0; i < password.length; i++) {
+    const ch = password.charAt(i)
+    if (!isNaN(+ch)) countOfNumbers++
+    else if (containsUppercase(ch)) countOfUpperCase++
+    else if (containsLowercase(ch)) countOfLowerCase++
+    else if (containsSpecialChar(ch)) countOfSpecialChar++
+  }
+
+  const errObj = {
+    upperCase: { pass: true, message: 'Add upper case' },
+    lowerCase: { pass: true, message: 'Add lower case' },
+    specialCh: { pass: true, message: 'Add special character' },
+    totalNumber: { pass: true, message: 'Add number' },
+  }
+
+  if (countOfLowerCase < 1) {
+    ctx.addIssue({
+      code: 'custom',
+      message: errObj.lowerCase.message,
+    })
+  }
+  if (countOfNumbers < 1) {
+    ctx.addIssue({
+      code: 'custom',
+      message: errObj.totalNumber.message,
+    })
+  }
+  if (countOfUpperCase < 1) {
+    ctx.addIssue({
+      code: 'custom',
+      message: errObj.upperCase.message,
+    })
+  }
+  if (countOfSpecialChar < 1) {
+    ctx.addIssue({
+      code: 'custom',
+      message: errObj.specialCh.message,
+    })
+  }
+}
+
 export const schema = z
   .object({
     name: z.string().refine(
@@ -13,12 +64,14 @@ export const schema = z
     ),
     age: z.string().refine((age) => Number(age) > 0, { message: 'Age should be positive number' }),
     email: z.string().email(),
-    password: z.string(),
-    confirmPassword: z.string(),
-    gender: z.union([z.literal('male'), z.literal('female')], {
-      message: 'Please choose either male or female',
+    password: z.string().superRefine(validatePasswordFieldInPlace),
+    confirmPassword: z.string().superRefine(validatePasswordFieldInPlace),
+    gender: z.string({
+      errorMap: () => ({ message: 'Please pick male or female' }),
     }),
-    termsAndContions: z.literal(true),
+    termsAndContions: z.literal(true, {
+      errorMap: () => ({ message: 'Please accept terms and conditions' }),
+    }),
     avatar: z
       .custom<FileList>()
       .transform((value) => {
@@ -41,59 +94,8 @@ export const schema = z
       .string()
       .refine((value) => countryNames.includes(value), 'We do not operate in that country'),
   })
-  .superRefine(({ password }, checkPassComplexity) => {
-    const containsUppercase = (ch: string) => /[A-Z]/.test(ch)
-    const containsLowercase = (ch: string) => /[a-z]/.test(ch)
-    const containsSpecialChar = (ch: string) => /[`!@#$%^&*()_\-+=[\]{};':"\\|,.<>/?~ ]/.test(ch)
-    let countOfUpperCase = 0,
-      countOfLowerCase = 0,
-      countOfNumbers = 0,
-      countOfSpecialChar = 0
 
-    for (let i = 0; i < password.length; i++) {
-      const ch = password.charAt(i)
-      if (!isNaN(+ch)) countOfNumbers++
-      else if (containsUppercase(ch)) countOfUpperCase++
-      else if (containsLowercase(ch)) countOfLowerCase++
-      else if (containsSpecialChar(ch)) countOfSpecialChar++
-    }
-
-    const errObj = {
-      upperCase: { pass: true, message: 'Add upper case' },
-      lowerCase: { pass: true, message: 'Add lower case' },
-      specialCh: { pass: true, message: 'Add special character' },
-      totalNumber: { pass: true, message: 'Add number' },
-    }
-
-    if (countOfLowerCase < 1) {
-      checkPassComplexity.addIssue({
-        code: 'custom',
-        path: ['password'],
-        message: errObj.lowerCase.message,
-      })
-    }
-    if (countOfNumbers < 1) {
-      checkPassComplexity.addIssue({
-        code: 'custom',
-        path: ['password'],
-        message: errObj.totalNumber.message,
-      })
-    }
-    if (countOfUpperCase < 1) {
-      checkPassComplexity.addIssue({
-        code: 'custom',
-        path: ['password'],
-        message: errObj.upperCase.message,
-      })
-    }
-    if (countOfSpecialChar < 1) {
-      checkPassComplexity.addIssue({
-        code: 'custom',
-        path: ['password'],
-        message: errObj.specialCh.message,
-      })
-    }
-  })
+  // the rule will be applied only after the whole object (with other field) passes the validation
   .superRefine(({ confirmPassword, password }, ctx) => {
     if (confirmPassword !== password) {
       ctx.addIssue({
