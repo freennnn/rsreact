3bb2a92a5830180a085336988e202d0bdbc8efb5
Alex Pranevich <freen.public@gmail.com>
2026-05-04 22:15:12 +0200

    feat: remove sorting by stars - cause it breaks relevance of results to query

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ const DEFAULT_SEARCH_QUERY = 'allitems';
 const PATH_BASE = 'https://api.github.com';
 const PATH_SEARCH = '/search/repositories';
 const PARAM_SEARCH = 'q=';
-const PARAMS_ADDITIONAL = '&sort=stars&page=1&per_page=3';
+const PARAMS_ADDITIONAL = '&page=1&per_page=3';
 
 export interface GitHubSearchResponse {
   items: Array<{
