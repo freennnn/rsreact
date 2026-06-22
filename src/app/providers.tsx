2ed5661b2dff81f844a415db9fc01e89757656fb
Alex Pranevich <freen.public@gmail.com>
2026-06-22 23:48:50 +0200

    feat: cleanup

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,13 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import { useState } from 'react';
-
-import { QueryClientProvider } from '@tanstack/react-query';
 
 import { ThemeProvider } from '../context/ThemeContext';
-import { createQueryClient } from '../query/queryClient';
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  const [queryClient] = useState(() => createQueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>{children}</ThemeProvider>
-    </QueryClientProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
